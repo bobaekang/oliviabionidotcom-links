cb1c29a88e707bd821680944b16c9d1163d481ed
bobaekang <bkang.dev@gmail.com>
2020-08-16 19:40:22 -0500

    Create Profile component

---
+++ src/components/Profile.svelte
@@ -0,0 +1,9 @@
+<script>
+  import BuyMeACoffeeButton from './BuyMeACoffeeButton.svelte'
+</script>
+
+<div class="flex flex-col items-center" style="width: 250px;">
+  <img class="rounded-full" src="assets/avatar.jpg" alt="avatar" />
+  <img class="my-4" src="assets/logo.svg" alt="logo" style="width: 200px;" />
+  <BuyMeACoffeeButton />
+</div>
