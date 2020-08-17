e8839dbde719ec4d06b705252db8eb73a6f9b651
bobaekang <bkang.dev@gmail.com>
2020-08-16 20:04:03 -0500

    Create config file containing links information

---
+++ src/config.js
@@ -0,0 +1,98 @@
+export const homeLinks = [
+  {
+    avatar: 'assets/ptm.jpg',
+    href: 'https://pilatesteachersmanual.oliviabioni.com',
+    newtab: true,
+    title: 'Listen to Pilates Teachers’ Manual podcast 🎧',
+  },
+  {
+    avatar: 'assets/psm.jpg',
+    href: 'https://pilatesstudentsmanual.oliviabioni.com',
+    newtab: true,
+    title: 'Listen to Pilates Students’ Manual podcast 🎧',
+  },
+  {
+    avatar: 'assets/discount.svg',
+    href: '/affiliates',
+    newtab: false,
+    title: 'Save on products I love ❤️',
+  },
+  {
+    avatar: 'assets/instagram.png',
+    href: 'https://www.instagram.com/pilatesteachersmanual/',
+    newtab: true,
+    title: 'Follow @PilatesTeachersManual',
+  },
+  {
+    avatar: 'assets/instagram.png',
+    href: 'https://www.instagram.com/pilatesstudentsmanual/',
+    newtab: true,
+    title: 'Follow @PilatesStudentsManual',
+  },
+  {
+    avatar: 'assets/instagram.png',
+    href: 'https://www.instagram.com/oliviabioniwellness/',
+    newtab: true,
+    title: 'Follow me @OliviaBioniWellness',
+  },
+  {
+    avatar: 'favicon.ico',
+    href: 'https://wellness.oliviabioni.com',
+    newtab: true,
+    title: 'Check out my website ✨',
+  },
+]
+
+export const affiliateLinks = [
+  {
+    avatar: 'assets/fabletics.svg',
+    href: 'https://www.fabletics.com/index.cfm?action=home.main&code=82773736',
+    newtab: true,
+    title: 'Fabletics activewear',
+    detail: 'Get your first two pairs of leggings for $24!',
+  },
+  {
+    avatar: 'assets/ritual.svg',
+    href: 'https://share.ritual.com/x/FktD8j',
+    newtab: true,
+    title: 'Ritual multivitamin',
+    detail:
+      '$15 off your first month of minty vitamins! First 10 sign ups only.',
+  },
+  {
+    avatar: 'assets/kenko.svg',
+    href: 'https://www.kencko.com/?referrer_id=2840563122236',
+    newtab: true,
+    title: 'Kenko smoothie',
+    detail:
+      '$10 off your first purchase of this delicious, portable smoothie subscription!',
+  },
+  {
+    avatar: 'assets/winc.png',
+    href: 'http://www.winc.com/hi/obioni',
+    newtab: true,
+    title: 'Winc wine subscription',
+    detail: '$22 off your first order of wine!',
+  },
+  {
+    avatar: 'assets/misfits.svg',
+    href: 'https://www.misfitsmarket.com/',
+    newtab: true,
+    title: 'Misfits Market organic produce',
+    detail: '25% off of your first order! Use discount code: COOKWME-QO3TQM',
+  },
+  {
+    avatar: 'assets/packagefree.png',
+    href: 'http://rwrd.io/iamkmza',
+    newtab: true,
+    title: 'Package Free zero waste shopping',
+    detail: '$10 off your first order and go green!',
+  },
+  {
+    avatar: 'assets/boka.svg',
+    href: 'https://go.referralcandy.com/share/3H4X728',
+    newtab: true,
+    title: 'Boka toothpaste',
+    detail: '15% off of your first order!',
+  },
+]
