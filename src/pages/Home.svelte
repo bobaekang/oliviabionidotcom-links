f403eda87b5abec3fbfbe8cc185bd885a6bd3a51
bobaekang <bkang.dev@gmail.com>
2020-08-16 20:04:31 -0500

    Complete Home page

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,1 +1,18 @@
-<h1>Home</h1>
+<script>
+  import Profile from '../components/Profile.svelte'
+  import BuyMeACoffeeButton from '../components/BuyMeACoffeeButton.svelte'
+  import LinkButton from '../components/LinkButton.svelte'
+  import { homeLinks } from '../config'
+</script>
+
+<div class="flex flex-wrap justify-center">
+  <div class="mt-8 mb-4">
+    <Profile />
+  </div>
+
+  <div class="w-full">
+    {#each homeLinks as link}
+      <LinkButton {...link} />
+    {/each}
+  </div>
+</div>
