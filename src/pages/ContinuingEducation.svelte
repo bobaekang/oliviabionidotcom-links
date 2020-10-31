869ef4f60c7a3388de1d7a1a062d84cf8252ffe6
bobaekang <bkang.dev@gmail.com>
2020-10-31 17:11:21 -0500

    Create continuing education page

---
+++ src/pages/ContinuingEducation.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Layout from '../Layout.svelte'
+  import { continuingEducationLinks as links } from '../config'
+
+  const descriptionHtml =
+    'My favorite programs for earning Continuing Education Credits. 👍<br /> I might get commissions from the following links.'
+</script>
+
+<Layout {descriptionHtml} {links} />
