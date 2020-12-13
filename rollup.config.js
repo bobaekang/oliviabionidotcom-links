3ff24115d0d5216bf6be62ea1c7e57c44a387bdb
bobaekang <bkang.dev@gmail.com>
2020-12-13 11:53:37 -0600

    Update rollup-svelte-plugin options

--- rollup.config.js
+++ rollup.config.js
@@ -41,13 +41,16 @@ export default {
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css: (css) => {
-        css.write('bundle.css')
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
+        // we'll extract any component CSS out into
+        // a separate file - better for performance
+        css: (css) => {
+          css.write('bundle.css')
+        },
       },
+      emitCss: false,
     }),
 
     // If you have external dependencies installed from
