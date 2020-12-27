ba3a7b0acacd1ef62af23aac258267242604be6b
bobaekang <bkang.dev@gmail.com>
2020-12-27 09:52:22 -0600

    Remove ritual from affiliates

--- src/pages/affiliates.js
+++ src/pages/affiliates.js
@@ -13,14 +13,6 @@ export default {
         detail: 'Get your first two pairs of leggings for $24!',
       },
       {
-        avatar: 'assets/ritual.svg',
-        href: 'https://share.ritual.com/x/FktD8j',
-        external: true,
-        title: 'Ritual multivitamin',
-        detail:
-          '$15 off your first month of minty vitamins! First 10 sign ups only.',
-      },
-      {
         avatar: 'assets/kenko.svg',
         href: 'https://www.kencko.com/?referrer_id=2840563122236',
         external: true,
