8bf1cba9fd83d4cfc6618eaf28e1444986f07896
bobaekang <bkang.dev@gmail.com>
2021-05-15 17:43:17 -0500

    Add banner to config

--- src/config.js
+++ src/config.js
@@ -5,6 +5,13 @@ import pilatesStudentsManual from './pages/pilatesStudentsManual'
 import pilatesTeachersManual from './pages/pilatesTeachersManual'
 
 export default {
+  banner: {
+    message: '📢 Help Olivia upgrade her podcast studio!',
+    href:
+      'https://www.buymeacoffee.com/oliviapodcasts/help-olivia-upgrade-her-podcast-studio',
+    mainLabel: 'Sign me up!',
+    closeLabel: 'Maybe later...',
+  },
   page: {
     affiliates,
     continuingEducation,
