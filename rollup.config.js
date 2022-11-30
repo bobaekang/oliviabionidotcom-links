09dc1a8c0b220b481395b0481f450b05691c0038
bobaekang <bkang.dev@gmail.com>
2022-11-29 20:10:47 -0600

    Remove problematic svelte plugin config

--- rollup.config.js
+++ rollup.config.js
@@ -44,11 +44,6 @@ export default {
       compilerOptions: {
         // enable run-time checks when not in production
         dev: !production,
-        // we'll extract any component CSS out into
-        // a separate file - better for performance
-        css: (css) => {
-          css.write('bundle.css')
-        },
       },
       emitCss: false,
     }),
