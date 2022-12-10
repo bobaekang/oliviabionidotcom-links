26b28ebaec1cf727902154377e95b7ce92fa8dd0
bobaekang <bkang.dev@gmail.com>
2022-12-10 14:31:27 -0600

    Remove featured links for completed workshop

--- src/pages/continuingEducation.js
+++ src/pages/continuingEducation.js
@@ -3,13 +3,6 @@ export default {
   props: {
     descriptionHtml:
       'My favorite programs for earning Continuing Education Credits. 👍<br /> I might get commissions from the following links.',
-    featuredLink: {
-      avatar: 'assets/npcp-favicon.png',
-      href: 'https://www.buymeacoffee.com/oliviapodcasts/e/103875',
-      external: true,
-      title: 'Creating An Inclusive Class For Prenatal Clients by Olivia Bioni',
-      detail: 'Earn 3 CECs with me. Buy Me A Coffee members get 50% off!',
-    },
     links: [
       {
         avatar: 'assets/chelseacorley.png',
