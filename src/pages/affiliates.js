de3de0d99f8b2759ecaa5f3f583ac14b33df19be
bobaekang <bkang.dev@gmail.com>
2024-10-27 09:03:28 -0500

    Fix typo

--- src/pages/affiliates.js
+++ src/pages/affiliates.js
@@ -6,17 +6,16 @@ export default {
     links: [
       {
         avatar: 'assets/fabletics.svg',
-        href:
-          'https://www.fabletics.com/index.cfm?action=home.main&code=82773736',
+        href: 'https://www.fabletics.com/index.cfm?action=home.main&code=82773736',
         external: true,
         title: 'Fabletics activewear',
         detail: 'Get your first two pairs of leggings for $24!',
       },
       {
-        avatar: 'assets/kenko.svg',
+        avatar: 'assets/kencko.svg',
         href: 'https://www.kencko.com/?referrer_id=2840563122236',
         external: true,
-        title: 'Kenko smoothie',
+        title: 'Kencko smoothie',
         detail:
           '$10 off your first purchase of this delicious, portable smoothie subscription!',
       },
@@ -29,8 +28,7 @@ export default {
       },
       {
         avatar: 'assets/girlfriendcollective.webp',
-        href:
-          'https://www.girlfriend.com/?sref_id=qccf4x7&utm_campaign=referral_program&utm_source=loyalty',
+        href: 'https://www.girlfriend.com/?sref_id=qccf4x7&utm_campaign=referral_program&utm_source=loyalty',
         external: true,
         title: 'Girlfriend Collective activewear',
         detail: '$10 off your first purchase of ethically made activewear!',
